786af280eec28556b8112a691f8dfc5163904fc1
risherlock <rishav-mani.sharma@stud-mail.uni-wuerzburg.de>
2024-07-10 15:42:59 +0200

    good soft docking without control

--- satellite/utils.h
+++ satellite/utils.h
@@ -1,4 +1,4 @@
-// Generally useful utility files for TAMARIW mission
+// Generally useful utility functions for TAMARIW mission
 // 2024-01-19
 
 #ifndef _TAMAWIW_UTILS_H_
@@ -8,5 +8,12 @@
 #define D2R 0.01745329251
 
 int sign(const float in);
+
+inline void swap(float *a, float *b)
+{
+  float temp = *a;
+  *a = *b;
+  *b = temp;
+}
 
 #endif // utils.h
--- threads/collision_control.cpp
+++ threads/collision_control.cpp
@@ -14,6 +14,7 @@ pid dpid[4]; // Distance PID
 float dsp = 100.0; // Distance setpoint, mm
 bool control_mode = false; // true: control and false: pull mode
 static double time = 0; // Thread timekeeper
+bool first_time = true;
 
 void collision_control_thread::init()
 {
@@ -23,6 +24,26 @@ void collision_control_thread::init()
     dpid[i].set_ki(PID_DISTANCE_KI);
     dpid[i].set_control_limits(PID_DISTANCE_UMIN, PID_DISTANCE_UMAX);
   }
+}
+
+// ToF values mean by limiting extreme values.
+// Returns 0.5(b + c) where data is sorted as a<b<c<d.
+float winsorized_mean(const int d[4])
+{
+  float arr[4];
+
+  for (int i = 0; i < 4; i++) {
+    arr[i] = (float)d[i];
+  }
+
+  // Sorting the necessary elements to find the middle two
+  if (arr[0] > arr[1]) swap(&arr[0], &arr[1]);
+  if (arr[2] > arr[3]) swap(&arr[2], &arr[3]);
+  if (arr[0] > arr[2]) swap(&arr[0], &arr[2]);
+  if (arr[1] > arr[3]) swap(&arr[1], &arr[3]);
+  if (arr[1] > arr[2]) swap(&arr[1], &arr[2]);
+
+  return (arr[1] + arr[2]) / 2.0;
 }
 
 void collision_control_thread::run()
@@ -38,6 +59,7 @@ void collision_control_thread::run()
         dpid[i].reset_memory();
       }
 
+      first_time = true;
       tx_tof.dt = 0.0;
       topic_collision_ctrl.publish(tx_tof);
       suspendCallerUntil(END_OF_TIME);
@@ -46,20 +68,55 @@ void collision_control_thread::run()
     // Read relative distance and velocity
     cb_tof.getOnlyIfNewData(rx_tof);
     const int d[4] = {rx_tof.d[0], rx_tof.d[1], rx_tof.d[2], rx_tof.d[3]};
+    float dmean = winsorized_mean(d);
+
     // float v[4] = {rx_tof.v[0], rx_tof.v[1], rx_tof.v[2], rx_tof.v[3]};
 
     // Perform distance control
-    if(control_mode)
+    // if(control_mode)
+    // {
+    //   //  for(uint8_t i = 0; i < 4; i++)
+    //   //  {
+    //   //    float error = dsp - d[i];
+    //   //    float isq = dpid[i].update(error, period / 1000.0); // Current squared
+    //   //    tx_current.i[i] = sign(isq) * sqrt(fabs(isq)); // Signed current
+    //   //  }
+    // }
+    // else
+    // {
+    //   tx_current.i[0] = -2500;
+    //   tx_current.i[1] = -2500;
+    //   tx_current.i[2] = -2500;
+    //   tx_current.i[3] = -2500;
+    // }
+
+    if(dmean < 100.0)
     {
-       for(uint8_t i = 0; i < 4; i++)
-       {
-         float error = dsp - d[i];
-         float isq = dpid[i].update(error, period / 1000.0); // Current squared
-         tx_current.i[i] = sign(isq) * sqrt(fabs(isq)); // Signed current
-       }
+      if(first_time)
+      {
+        tx_current.i[0] = 1000;
+        tx_current.i[1] = 1000;
+        tx_current.i[2] = 1000;
+        tx_current.i[3] = 1000;
+        first_time = false;
+      }
+
+      tx_current.i[0] = -700;
+      tx_current.i[1] = -700;
+      tx_current.i[2] = -700;
+      tx_current.i[3] = -700;
+
+      if(dmean < 40.0)
+      {
+        tx_current.i[0] = -1500;
+        tx_current.i[1] = -1500;
+        tx_current.i[2] = -1500;
+        tx_current.i[3] = -1500;
+      }
     }
     else
     {
+      first_time = true;
       tx_current.i[0] = -2500;
       tx_current.i[1] = -2500;
       tx_current.i[2] = -2500;
